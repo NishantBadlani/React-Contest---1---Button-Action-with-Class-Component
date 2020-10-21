197aa7cf6e8406ba99ec9fde5f52fceefa4f4305
NishantBadlani <nishant.03badlani@gmail.com>
2020-10-21 21:02:20 +0530

    .

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,17 @@
-import React, {Component, useState} from "react"
-import "../styles/App.css"
+import React, {Component, useState} from "react";
+import "../styles/App.css";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       showParagraph: false
-    }
+    };
   }
 
   handleClick = () => {
-    this.setState({showParagraph: true})
-  }
+    this.setState({showParagraph: true});
+  };
 
   render() {
     return (
@@ -21,8 +21,8 @@ class App extends Component {
         </button>
         {this.state.showParagraph ? <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> : null}
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
